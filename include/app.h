b8d7a5fbf329de5c6da74653eeb1c75f3cae8aae
Aaron Paden <aaronbpaden@gmail.com>
2025-02-02 15:09:39 -0600

    Init SDL window.

---
+++ include/app.h
@@ -0,0 +1,10 @@
+#ifndef _app_h_
+#define _app_h_
+
+#include <SDL3/SDL.h>
+
+typedef struct {
+	SDL_Window *window;
+} app_t;
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,6 +1,52 @@
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL.h>
+
+#include <stdlib.h>
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World."); 
-    return 0;
+#include "app.h"
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+{
+	app_t* state = malloc(sizeof(app_t));
+	*appstate = state;
+
+	if (!SDL_InitSubSystem(SDL_INIT_VIDEO|SDL_INIT_EVENTS))
+	{
+		SDL_Log("%s\n", SDL_GetError());
+		return SDL_APP_FAILURE;
+	}
+
+	state->window = SDL_CreateWindow(
+			"Retro ADV",
+			800,
+			600,
+			0);
+
+	return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+	return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+	switch (event->type)
+	{
+		case SDL_EVENT_QUIT:
+			return SDL_APP_SUCCESS;
+	}
+
+	return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+	app_t *state = (app_t *)appstate;
+	SDL_DestroyWindow(state->window);
+	free(appstate);
+	return;
 }
