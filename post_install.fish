162aae4485cd3e646a680f51f23e8ec570b3de1d
Aaron Paden <aaronbpaden@gmail.com>
2025-03-02 08:14:41 -0600

    Let's use msys2's packages for now.
    Eventually we will move to vendored packages with conan, but conan's
    SDL3 package is still WIP. Meson does not have wraps at all for SDL3.

--- post_install.fish
+++ post_install.fish
@@ -10,14 +10,5 @@ end
 cd (string replace "C:" "/c/" $MESON_INSTALL_DESTDIR_PREFIX)
 pwd
 
-rm -vf bin/*.a
-rm -vf bin/showanim.exe
-rm -vf bin/showimage.exe
-#rm -rvf lib
-mv lib/* bin
-rmdir lib
-
-#Why is this needed??
-cp bin/libSDL3_shared.dll bin/SDL3.dll
-
-printf "@echo off\ncd bin\nretroadv.exe >log.txt 2>&1 5>&1" >launch.bat
+cd bin
+cp /clang64/bin/SDL3*.dll .
